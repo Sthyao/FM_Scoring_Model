0d5699c5432a922b7f7994b066e73f90745e86bd
sthyao <qq1171098650@163.com>
2022-04-22 09:54:28 +0800

    no message

--- SRG.py
+++ SRG.py
@@ -1,3 +1,4 @@
+from logging import basicConfig
 import numpy as np
 import matplotlib.pyplot as plt
 from sympy import *
@@ -16,7 +17,6 @@ def SRG(m_init,d=2):
     #d is the dimension of implicit vectors
     m,n = m_init.shape
     xvars = mat_structure(m,n,d)
-
     ans_list = []
     d_list = []
 
@@ -25,16 +25,18 @@ def SRG(m_init,d=2):
     ind2 = [x for x in range(m,n+m)]
 
     #To avoid singular matrices, we need to modify the constraints
-    x_14, x_13 = symbols('x_14 x_13')
+    
     fx = sum([s**2 for s in (xvars.row(ind1)*(xvars.row(ind2).T) - m_init)])
-    print(fx)
     hxs = [sum(xvars.row(ind1)*(xvars.row(ind2).T) - m_init),
-    sum(xvars.row(ind1).col(0))-m+x_13,
-    sum(xvars.row(ind1).col(1))-m-x_14]
+    sum(xvars.row(ind1).col(0))-m,    
+    sum(xvars.row(ind1).col(1))-m]
 
-    xcurr = [1.5,0.5, 0.5, 0.3, 1.5, 1.7, 1, 1.5, 1, 3, 2, 1, 3, 1]
+    xcurr = [1.5,0.5, 0.5, 0.3, 1.5, 1.7, 1, 1.5, 1, 3, 2, 1, 3, 1,3,2]
     xcurr = np.array(xcurr)
-    x_l = np.zeros(len(xcurr))
+    x_l1 = np.zeros(len(xcurr))
+    x_l1[-1] = 0.01
+    x_l1[-2] = 0.01
+    x_l2 = np.zeros(len(xcurr))
     
     # Parameter initializations
     # The parameters of the fastest descent and the parameters of the gradient method
@@ -45,14 +47,19 @@ def SRG(m_init,d=2):
     alpha_norm = 0.003									
     
     max_iter = 10
-    max_outer_iter = 30
+    max_outer_iter = 2
     eps = 0.001
 
     #Generates a list of stored derivatives
     dfx = np.array([diff(fx, xvar) for xvar in xvars])
     dhxs = np.array([[diff(hx, xvar) for xvar in xvars] for hx in hxs])
     nonbasic_vars = len(xvars) - len(hxs)
+    basic_vars = len(hxs)
     opt_sols = []
+
+    #Divide constrained and unconstrained variables
+    constrain_ind = [x for x in range(m*2-basic_vars,m)]
+    nonconstrain_ind = [x for x in range((m+n)*2)]
 
     for outer_iter in range(max_outer_iter):
 
@@ -63,13 +70,13 @@ def SRG(m_init,d=2):
         #The gradient of the objective function, numeralization
         #The gradient of the constraint function,numeralization
         delta_f = np.array([df.subs(zip(xvars, xcurr)) for df in dfx])
-        delta_h = np.array([[dh.subs(zip(xvars, xcurr)) for dh in dhx] for dhx in dhxs])		
+        delta_h = np.array([[dh.subs(zip(xvars, xcurr)) for dh in dhx] for dhx in dhxs])	
         #J,C stored constrained and unconstrained gradients respectively, numerically
-        J = np.array([dhx[nonbasic_vars:] for dhx in delta_h])								
-        C = np.array([dhx[:nonbasic_vars] for dhx in delta_h])
+        J = np.array([dhx[constrain_ind] for dhx in delta_h])								
+        C = np.array([dhx[nonconstrain_ind]] for dhx in delta_h])
         #same to J,C
-        delta_f_bar = delta_f[nonbasic_vars:]
-        delta_f_cap = delta_f[:nonbasic_vars]
+        delta_f_bar = delta_f[constrain_ind]
+        delta_f_cap = delta_f[nonconstrain_ind]
         #Find the inverse of constrained gradient matrix
         J_inv = np.linalg.inv(np.array(J, dtype=float))
         #dz = delta f - delta f * J^-1 * C
@@ -85,20 +92,25 @@ def SRG(m_init,d=2):
         d = np.concatenate((d_bar, d_cap)).T
         d_list.append([float(d[i]) for i in range(len(d))])
 
-        #alpha = alpha_fast
-        alpha = alpha_norm
+        alpha = alpha_fast
+        #alpha = alpha_norm
 
-        #while alpha < 0.000001:
-        count1 = 0
-        while count1 < 100:
-
+        while alpha > 0.000001:
+        #count1 = 0
+        #while count1 < 100:
+            #count1 += 1
             #print( '\nAlpha value: {0}\n'.format(alpha))
             v = xcurr.T + alpha * d
+            
             #Upper and lower limit conditions
-            #v = np.array([x_l[i] if v[i]<x_l[i] else v[i] for i in range(len(v))])
-            #v = np.array([x_l[i] if v[i]>x_l[i] else v[i] for i in range(len(v))])
-            v_bar = v[:nonbasic_vars]
-            v_cap = v[nonbasic_vars:]
+            v[-1] = -0.01 if v[-1] < -0.01 else v[-1]
+            v[-2] = -0.01 if v[-2] < -0.01 else v[-2]
+            v[-1] = 0.01 if v[-1] > 0.01 else v[-1]
+            v[-2] = 0.01 if v[-2] > 0.01 else v[-2]
+            
+            #print(v)
+            v_bar = v[constrain_ind]
+            v_cap = v[nonconstrain_ind]
             flag = False
 
             #Fast gradient descent
@@ -106,10 +118,11 @@ def SRG(m_init,d=2):
                 #print( 'Iteration: {0}, optimal solution obtained at x = {1}'.format(iters + 1, v))
                 h = np.array([hx.subs(zip(xvars, v)) for hx in hxs])
                 # Check if hx satisfies all constraints, it means our constraints are satisfied in the form of derivatives
+                #print(h)
                 if all([abs(h_i) < eps for h_i in h]):
                     #Better or not				
                     if fx.subs(zip(xvars, xcurr)) <= fx.subs(zip(xvars, v)):
-                        alpha = alpha * gamma
+                        alpha = alpha * speed_fast
                         break
                     else:
                         xcurr = v 						
@@ -117,11 +130,13 @@ def SRG(m_init,d=2):
                         break
 
                 delta_h_v = np.array([[dh.subs(zip(xvars, v)) for dh in dhx] for dhx in dhxs])
-                J_inv_v = np.linalg.inv(np.array([dhx[nonbasic_vars:] for dhx in delta_h_v], dtype=float))
-                v_next_cap = v_cap - J_inv_v.dot(h)
-                #Upper and lower limit conditions
-                #v_next_cap = np.array([x_l[i] if v_next_cap[i]<x_l[i] else v_next_cap[i] for i in range(len(v_next_cap))])
-
+                J_inv_v = np.linalg.inv(np.array([dhx[nonconstrain_ind] for dhx in delta_h_v], dtype=float))
+                v_next_cap = v_cap - J_inv_v.dot(h)   
+                print(v_next_cap)
+                #v_next_cap[-1] = -0.01 if v_next_cap[-1] < -0.01 else v_next_cap[-1]
+                #v_next_cap[-2] = -0.01 if v_next_cap[-2] < -0.01 else v_next_cap[-2]
+                #v_next_cap[-1] = 0.01 if v_next_cap[-1] > 0.01 else v_next_cap[-1]
+                #v_next_cap[-2] = 0.01 if v_next_cap[-2] > 0.01 else v_next_cap[-2]
                 if abs(np.linalg.norm(np.array(v_cap - v_next_cap, dtype=float), 1)) > eps:
                     v_cap = v_next_cap
                     v = np.concatenate((v_bar, v_cap))
@@ -132,14 +147,14 @@ def SRG(m_init,d=2):
                     if all([abs(h_i) < eps for h_i in h]):
                         #Search for lower values of alpha, in steepest descent method
                         if fx.subs(zip(xvars, xcurr)) <= fx.subs(zip(xvars, v)):
-                            #alpha = alpha * speed_fast				
+                            alpha = alpha * speed_fast				
                             break
                         else:
                             xcurr = v
                             flag = True
                             break
                     else:
-                        #alpha = alpha * speed_fast
+                        alpha = alpha * speed_fast
                         break
             if flag == True:
                 break
